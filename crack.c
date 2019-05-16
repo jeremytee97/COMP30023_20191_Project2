390c6ca91127f6e9533e7585c7b7b82b27b58ba2
Jeremy <jeremytee97@gmail.com>
2019-05-17 00:07:03 +1000

    add implementation of 6 keyword

--- crack.c
+++ crack.c
@@ -21,25 +21,30 @@ static const int MAX_LENGTH = 4;
 // the file I/O will dominate the test time.
 
 const BYTE alphabet[MAX_KEYWORDS] = "abcdefghijklmnopqrstuvwxyz"
-                       "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                       "0123456789"
                        " ,./;'[]\\-=`<>?:\"{}|~!@#$%^&*()_+"
-                       "0123456789";
+                       "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
+/* const BYTE similarCharacters = {'a', 'A', '4', '@'}, {'b', 'B', '?', '6'}, {'c', 'C', '('}, {'d', 'D', '?'}, {'e', 'E', '3'},
+{'f', 'F'}, {'g', 'G'}, {''}
+ */
+//" ,./;'[]\\-=`<>?:\"{}|~!@#$%^&*()_+"
 int main(int argc, char *argv[])
 {
     if (argc < 2) {
-        fprintf(stderr, "Usage: %s Length\n", argv[0]);
-        exit(1);
+      BYTE** file = readHashFile(PWD4_FILENAME, NUM_PWD4SHA256);
+      generateFourCharPass(MAX_LENGTH, file, NUM_PWD4SHA256);
+      free(file);
+      printf("END OF KEYWORD 4\n");
+      BYTE** file2 = readHashFile(PWD6_FILENAME, NUM_PWD6SHA256);
+      generateSixCharPass(6, file2, NUM_PWD6SHA256);
+      free(file2);
     }
-
-    BYTE** file = readfile("pwd4sha256", NUM_PWD4SHA256);
-    generate(atoi(argv[1]), file);
-    
     return 0;
 }
 
 
-BYTE** readfile(char* filename, int num_hashes){
+BYTE** readHashFile(char* filename, int num_hashes){
    
     FILE *fp;
     BYTE **buffer = (BYTE **)malloc(num_hashes * sizeof(BYTE *)); 
@@ -60,16 +65,18 @@ BYTE** readfile(char* filename, int num_hashes){
 }
 
 
-void compareHashes(BYTE** file, BYTE* guess, int num_hashes, int guess_length){
+int compareHashes(BYTE** file, BYTE* guess, int num_hashes, int guess_length){
     int test;
     BYTE* hashed_guess = hashGuess(guess, guess_length);
     for(int i = 0; i < num_hashes; i++){
         test = memcmp(file[i], hashed_guess, SHA256_BLOCK_SIZE);
         if(test == 0){
             printf("%s %d\n", guess, i);
+            return 1;
         }
     }
     free(hashed_guess);
+    return 0;
 }
 
 BYTE* hashGuess(BYTE* guess, int guess_length){
@@ -83,7 +90,7 @@ BYTE* hashGuess(BYTE* guess, int guess_length){
 }
 
 /* Brute force generating all possible 4 keyword password */
-void generateFourCharPass(int maxlen, BYTE** file){
+void generateFourCharPass(int maxlen, BYTE** file, int numberOfHash){
     int   len      = maxlen;
     BYTE *buffer   = malloc((maxlen + 1));
 
@@ -100,7 +107,7 @@ void generateFourCharPass(int maxlen, BYTE** file){
 
     // Initialize buffer
     memset(buffer, '\0', bufLen);
-    int numOfGuesses = 0;
+    int numOfCorrectGuesses = 0;
     for(int i = 0; i < MAX_KEYWORDS; i++){
         for(int j = 0; j < MAX_KEYWORDS; j++){
             for(int k = 0; k < MAX_KEYWORDS; k++){
@@ -109,14 +116,99 @@ void generateFourCharPass(int maxlen, BYTE** file){
                     buffer[1] = alphabet[j];
                     buffer[2] = alphabet[k];
                     buffer[3] = alphabet[l];
-                    compareHashes(file, buffer, NUM_PWD4SHA256, MAX_LENGTH);
+                    numOfCorrectGuesses += compareHashes(file, buffer, NUM_PWD4SHA256, MAX_LENGTH);
                     memset(buffer, '\0', bufLen);
-                    numOfGuesses++;
+                    if(numOfCorrectGuesses == numberOfHash){
+                        free(buffer);
+                        return;
+                    }
                 }
             }
         }
     }
-    printf("Num of guesses %d\n", numOfGuesses);
+    // Clean up
+    free(buffer);
+}
+
+
+void smartGuesses(int maxlen, BYTE** file, int numOfHash){
+    BYTE *buffer   = malloc((maxlen + 1));
+
+    if (buffer == NULL) {
+        fprintf(stderr, "Cannot allocate memory for buffer");
+        exit(1);
+    }
+
+
+    // This for loop generates all 1 letter patterns, then 2 letters, etc,
+    // up to the given maxlen.
+    // The stride is one larger than len because each line has a '\0'.
+    int stride = maxlen+1;
+    int bufLen = stride;
+
+    // Initialize buffer
+    memset(buffer, '\0', bufLen);
+    int numOfCorrectGuesses = 0;
+
+    
+    //try dictionary attack first
+
+    char* filename = COMMON_PASS_FILENAME; /* should check that argc > 1 */
+    FILE* common_password = fopen(filename, "r"); /* should check the result */
+    char line[256];
+
+    fclose(common_password);
+
+}
+
+
+/* Brute force generating all possible 4 keyword password */
+void generateSixCharPass(int maxlen, BYTE** file, int numberOfHash){
+    int   len      = maxlen;
+    BYTE *buffer   = malloc((maxlen + 1));
+
+    if (buffer == NULL) {
+        fprintf(stderr, "Cannot allocate memory for buffer");
+        exit(1);
+    }
+
+    // This for loop generates all 1 letter patterns, then 2 letters, etc,
+    // up to the given maxlen.
+    // The stride is one larger than len because each line has a '\0'.
+    int stride = len+1;
+    int bufLen = stride;
+
+    BYTE test[7] = {'t', 'e', 'e', 'j', '1', ' '};
+    int numOfCorrectGuesses = 0;
+    numOfCorrectGuesses = compareHashes(file, test, NUM_PWD6SHA256, 6);
+
+    // Initialize buffer
+    memset(buffer, '\0', bufLen);
+    for(int i = 0; i < MAX_KEYWORDS; i++){
+        for(int j = 0; j < MAX_KEYWORDS; j++){
+            for(int k = 0; k < MAX_KEYWORDS; k++){
+                for(int l = 0; l < MAX_KEYWORDS; l++){
+                    for(int m = 0; m < MAX_KEYWORDS; m++){
+                        for(int n = 0; n < MAX_KEYWORDS; n++){
+                            buffer[0] = alphabet[i];
+                            buffer[1] = alphabet[j];
+                            buffer[2] = alphabet[k];
+                            buffer[3] = alphabet[l];
+                            buffer[4] = alphabet[m];
+                            buffer[5] = alphabet[n];
+                            numOfCorrectGuesses += compareHashes(file, buffer, NUM_PWD6SHA256, 6);
+                            //printf("%s\n", buffer);
+                            memset(buffer, '\0', bufLen);
+                            if(numOfCorrectGuesses == numberOfHash){
+                                free(buffer);
+                                return;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
     // Clean up
     free(buffer);
 }
--- crack.h
+++ crack.h
@@ -3,17 +3,22 @@
 #define CRACK_H
 
 #define MAX_KEYWORDS 95
+#define SMART_GUESS 90
 #define NUM_PWD4SHA256 10
 #define NUM_PWD6SHA256 20
 #define PWD4_FILENAME "pwd4sha256"
+#define PWD6_FILENAME "pwd6sha256"
+#define COMMON_PASS_FILENAME "common_passwords.txt"
 
 
 /*Function declaration*/
-void compareHashes(BYTE** file, BYTE* guess, int num_hashes, int guess_length);
+int compareHashes(BYTE** file, BYTE* guess, int num_hashes, int guess_length);
 
-void generateFourCharPass(int maxlen, BYTE** file);
+void generateFourCharPass(int maxlen, BYTE** file, int numberOfHash);
 
-BYTE** readfile(char* filename, int num_hashes);
+void generateSixCharPass(int maxlen, BYTE** file, int numberOfHash);
+
+BYTE** readHashFile(char* filename, int num_hashes);
 
 BYTE* hashGuess(BYTE* guess, int guess_length);
 
